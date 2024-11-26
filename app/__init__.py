37046af21d1018398dcef4e7d1d97f8ecc526f2e
ALUNO SESI CABO <aluno.sesicabo@SISTEMAFIEPE.LOCAL>
2024-11-26 18:02:59 -0300

    backup de teste

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
     
     from .import routes
     routes.init__app(app)
--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,43 @@
 from flask import render_template
+from.models import Product
 
 def init__app(app):
     @app.route('/')
     def index():
         return render_template('index.html')
+    
+    @app.route('/')
+    def index():
+        products = Product.query.all()  # Recupera todos os produtos
+        return render_template('index.html', products=products)
+    
+    # Rota para criar um novo produto
+    @app.route('/product/new', methods=['GET', 'POST'])
+    def new_product():
+        if request.method == 'POST':
+            name = request.form['name']
+            price = request.form['price']
+            product = Product(name=name, price=float(price))
+            db.session.add(product)
+            db.session.commit()
+            return redirect(url_for('main.index'))
+        return render_template('new_product.html')
+    
+    # Rota para editar um produto existente
+    @app.route('/product/edit/<int:id>', methods=['GET', 'POST'])
+    def edit_product(id):
+        product = Product.query.get_or_404(id)
+        if request.method == 'POST':
+            product.name = request.form['name']
+            product.price = float(request.form['price'])
+            db.session.commit()
+            return redirect(url_for('main.index'))
+        return render_template('edit_product.html', product=product)
+    
+    # Rota para excluir um produto
+    @app.route('/product/delete/<int:id>', methods=['POST'])
+    def delete_product(id):
+        product = Product.query.get_or_404(id)
+        db.session.delete(product)
+        db.session.commit()
+        return redirect(url_for('main.index'))
